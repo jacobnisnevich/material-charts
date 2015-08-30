f03c0d96c487b0d04d7280796718cbf92f38158a
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-30 14:36:52 -0700

    error checking and validation

--- js/material-charts.js
+++ js/material-charts.js
@@ -6,10 +6,15 @@ var MaterialCharts = {}
 
 MaterialCharts.bar = function( element, data ) {
 	initializeChartArea( element, data.height, data.width, data.background, data.shadowDepth );
-	insertTitle( element, data.title );
-	insertAxes( element );
-	insertData( element, parseInt(data.height), parseInt(data.width), data.datasets.values, Math.max.apply(null, data.datasets.values), data.datasets.labels, data.datasets.color );
-	alignLabels( element );
+	var validateResult = validateBarChartData( data );
+	if (validateResult.valid) {	
+		insertTitle( element, data.title );
+		insertAxes( element );
+		insertData( element, parseInt(data.height), parseInt(data.width), data.datasets.values, Math.max.apply(null, data.datasets.values), data.datasets.labels, data.datasets.color );
+		alignLabels( element );
+	} else {
+		insertErrorMessage( element, validateResult.message );
+	}
 }
 
 // Pie Charts
@@ -17,28 +22,40 @@ MaterialCharts.bar = function( element, data ) {
 MaterialCharts.pie = function( element, data ) {
 	initializeChartArea( element, data.height, data.width, data.background, data.shadowDepth );
 	insertTitle( element, data.title );
-	$(element).append("<div style='position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0; height: 18px; width: 200px'><b>Pie Charts Not Available Yet</b></div>")
+	insertErrorMessage( element, "Pie Charts Not Available Yet" );
 }
 
 //// Helper functions
 
 function initializeChartArea( element, height, width, background, shadowDepth ) {
-	$(element).addClass("chart-area");
+	$(element).addClass("material-charts-chart-area");
 	$(element).css("height", height);
 	$(element).css("width", width);
-	$(element).css("background-color", background);
-	$(element).addClass("shadow-" + shadowDepth);
+
+	if (background) {
+		$(element).css("background-color", background);
+	} else {
+		$(element).css("background-color", "transparent");
+	}
+
+	if (shadowDepth) {
+		$(element).addClass("material-charts-shadow-" + shadowDepth);
+	}
 }
 
 function insertTitle( element, title ) {
-	$(element).append("<div class='chart-title'>" + title + "</div>");
+	$(element).append("<div class='material-charts-chart-title'>" + title + "</div>");
+}
+
+function insertErrorMessage( element, message ) {
+	$(element).append("<div class='material-charts-error-message'><b>" + message + "</b></div>");
 }
 
 // Bar Charts
 
 function insertAxes( element ) {
-	$(element).append("<div class='box-chart-x-axis'></div>");
-	$(element).append("<div class='box-chart-y-axis'></div>");
+	$(element).append("<div class='material-charts-box-chart-x-axis'></div>");
+	$(element).append("<div class='material-charts-box-chart-y-axis'></div>");
 }
 
 function insertData( element, height, width, dataElements, dataMax, dataLabels, color ) {
@@ -67,16 +84,16 @@ function insertData( element, height, width, dataElements, dataMax, dataLabels, 
 }
 
 function insertVerticalTick( element, heightPos, label ) {
-	$(element).append("<div class='box-chart-vertical-tick' style='bottom: " + heightPos + "px;'></div>");
-	$(element).append("<div class='box-chart-vertical-tick-label' style='bottom: " + (heightPos - 4) + "px;'>" + label + "</div>");
+	$(element).append("<div class='material-charts-box-chart-vertical-tick' style='bottom: " + heightPos + "px;'></div>");
+	$(element).append("<div class='material-charts-box-chart-vertical-tick-label' style='bottom: " + (heightPos - 4) + "px;'>" + label + "</div>");
 }
 
 function insertHorizontalLabel( element, horizontalPos, label ) {
-	$(element).append("<div class='box-chart-horizontal-label' style='left: " + horizontalPos + "px;'>" + label + "</div>");
+	$(element).append("<div class='material-charts-box-chart-horizontal-label' style='left: " + horizontalPos + "px;'>" + label + "</div>");
 }
 
 function insertVerticalBar( element, horizontalPos, horizontalSpread, height, value, color ) {
-	$("<div class='box-chart-vertical-bar " + color + "' style='left: " + 
+	$("<div class='material-charts-box-chart-vertical-bar material-charts-" + color + "' style='left: " + 
 		(horizontalPos - horizontalSpread / 4) + "px; width: " + (horizontalSpread / 2) + 
 		"px; height: " + height + "px'></div>").appendTo($(element)).hide().slideDown();
 }
@@ -84,7 +101,7 @@ function insertVerticalBar( element, horizontalPos, horizontalSpread, height, va
 function alignLabels( element ) {
 	var width, oldPosition, newPosition;
 
-	$(element + " .box-chart-horizontal-label").each(function() {
+	$(element + " .material-charts-box-chart-horizontal-label").each(function() {
 		width = $(this).width();
 		oldPosition = parseInt($(this).css("left"));
 		newPosition = oldPosition - width / 2;
@@ -93,3 +110,52 @@ function alignLabels( element ) {
 }
 
 // Pie Charts
+
+//// Validation functions
+
+// Bar Charts
+
+function validateBarChartData( data ) {
+	var validateResult = {
+		"valid": true,
+		"message": ""
+	}
+
+	if (data.datasets.values.length == 0) {
+		validateResult.valid = false;
+		validateResult.message = "Material Charts Error: Dataset values cannot be empty.";
+		return validateResult;
+	}
+
+	if (data.datasets.labels.length == 0) {
+		validateResult.valid = false;
+		validateResult.message = "Material Charts Error: Dataset labels cannot be empty.";
+		return validateResult;
+	}
+
+	if (data.datasets.labels.length != data.datasets.values.length) {
+		validateResult.valid = false;
+		validateResult.message = "Material Charts Error: Dataset labels and values must be the same length.";
+		return validateResult;
+	}
+
+	if (data.datasets.color == null) {
+		validateResult.valid = false;
+		validateResult.message = "Material Charts Error: Dataset color must be specified.";
+		return validateResult;
+	}
+
+	if (data.title == null) {
+		validateResult.valid = false;
+		validateResult.message = "Material Charts Error: Chart data must have a title.";
+		return validateResult;
+	}
+
+	if (data.height == null || data.width == null) {
+		validateResult.valid = false;
+		validateResult.message = "Material Charts Error: Chart data must have a height and width.";
+		return validateResult;
+	}
+
+	return validateResult;
+}
